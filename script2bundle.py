fe8933450eecc0447ae915a1d1ba0d5772a09a93
Andy Thomas <andythomas@web.de>
2022-06-03 13:34:06 +0200

    Allow destination and app name

--- script2bundle.py
+++ script2bundle.py
@@ -5,8 +5,7 @@
 #
 
 import argparse  # cmd line parser
-import os  # mkdir and such
-# import the required packages
+import os
 import plistlib
 import shutil  # copy files
 import sys  # find the python3 path
@@ -37,27 +36,57 @@ parser = argparse.ArgumentParser(
     description='Generate an application bundle (Mac OS) from an executable.')
 
 # The options:
-parser.add_argument('-e', '--executable', type=str,
+parser.add_argument('-e', '--executable',
+                    type=str,
                     help='The existing executable file.')
 
-parser.add_argument('-i', '--icon', type=str,
+parser.add_argument('-i', '--icon',
+                    type=str,
                     help='The existing png icon file.')
 
+parser.add_argument('-d', '--destination',
+                    type=str,
+                    choices={'user', 'system', 'executable'},
+                    default='executable',
+                    const='executable',
+                    nargs='?',
+                    help='The destination of the .app file (default: %(default)s).')
+
+parser.add_argument('-n', '--name', type=str,
+                    help='The name of the app (default: EXECUTABLE.app).')
 
 # initiate the parsing
 args = parser.parse_args()
 
 # use the example if no file is given
 executable = args.executable
-
-if executable == None:
+if (executable == None):
     executable = 'example'
     with open(executable, 'w') as examplefile:
         examplefile.write(example)
     os.chmod(executable, 0o755)
 
-# First, generate the directory framework
-app_name = executable + '.app'
+# add the executable and the name to the Info.plist file
+name = args.name
+head, tail = os.path.split(executable)
+info_plist = dict(CFBundleExecutable=tail)
+if (name == None):
+    name = tail
+else:
+    name = args.name
+info_plist.update(CFBundleName=name)
+
+# Determine the destination of the .app file
+head, tail = os.path.split(executable)
+app_name = name + '.app'
+if (args.destination=='executable'):
+    app_name = os.path.join(head, app_name)
+elif (args.destination=='system'):
+    app_name = os.path.join('/Applications', app_name)
+elif (args.destination=='user'):
+    app_name = os.path.join(os.path.expanduser("~"), 'Applications', app_name)
+
+# Generate the directory framework
 contents_dir = os.path.join(app_name, 'Contents')
 macos_dir = os.path.join(contents_dir, 'MacOS')
 resources_dir = os.path.join(contents_dir, 'Resources')
@@ -66,11 +95,6 @@ os.makedirs(resources_dir, exist_ok=True)
 
 # copy the executable in the correct place
 shutil.copy(executable, macos_dir)
-
-# add the executable to the Info.plist file
-head, tail = os.path.split(executable)
-info_plist = dict(CFBundleExecutable=tail)
-info_plist.update(CFBundleName=tail)
 
 # deal with the optional icon file
 if (args.icon != None):
@@ -83,7 +107,7 @@ if (args.icon != None):
     # copy the icon file in the correct place
     shutil.copy(icns_file, resources_dir)
 
-
+# write the Info.plist file
 info_filename = os.path.join(contents_dir, 'Info.plist')
 with open(info_filename, 'wb') as infofile:
     plistlib.dump(info_plist, infofile)
