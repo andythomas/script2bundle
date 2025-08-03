f9dd98152ff9e71d3d66d8d10d9dd2094a4c3bf7
Andy Thomas <andythomas@web.de>
2025-08-03 18:12:56 +0200

    refactor: make internal structure more modular (#34)
    * add filesystem dictionary
    
    * add ApplicationBundle class
    
    * restore launch
    
    * restore destination and display name
    
    * recover icon
    
    * recover filename
    
    * recover terminal
    
    solves #29
    
    * recover type role
    
    * use original tests
    
    * fix remaining 2 pytest errors
    
    * add and update docstrings
    
    * reduce complexity and add linter rule
    
    * add pytest to PR checks
    
    * fix ci workflow
    
    * run tests on mac and install packages
    
    * define tests github can run
    
    * test longer sleep for ci
    
    * Revert "test longer sleep for ci"
    
    This reverts commit b76841f5ffe267e89be356ac580b4592e54c3604.
    
    * remove one more test for ci
    
    * update grep syntax for mac

--- script2bundle.py
+++ script2bundle.py
@@ -23,197 +23,318 @@ import shutil
 import string
 import sys
 import time
-from typing import Optional
+from io import BytesIO
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Union
 
 import icnsutil
 
 LAUNCHER_NAME = "terminallauncher"
 
 
-def _is_valid_domain(domain):
-    """Check the validity of the Uniform Type Identifiers."""
-    rfc1035_chars = string.ascii_lowercase + string.digits + "-."
-    if not all(char in rfc1035_chars for char in domain.lower()):
-        return False
-    if len(domain) > 253:
-        return False
-    if "--" in domain:
-        return False
-    if ".." in domain:
-        return False
-    return True
+class _FilesystemDictionary:
+    """Create files and folders in a dictionary."""
+
+    def __init__(self):
+        """Create the root directory."""
+        self.directory_dict = {}
+
+    def _relative_path(self, path: Path) -> Path:
+        """
+        Remove the leading / from a path (if existing).
+
+        Parameters
+        ----------
+        path: Path
+            The path to be cleaned from a prepending slash.
+
+        Returns
+        -------
+        Path
+            The 'relative' path.
+        """
+        if path.parts[0] == "/":
+            path = Path(*path.parts[1:])
+        return path
+
+    def _cd(self, path: Path) -> dict:
+        """
+        Change into a directory and create unexisting ones.
+
+        Parameters
+        ----------
+        path : Path
+            The directory to change into (starting from root).
+
+        Returns
+        -------
+        dict
+            The 'directory' that was changed into.
+        """
+        current_folder = self.directory_dict
+        for subfolder in path.parts:
+            if subfolder not in current_folder:
+                current_folder[subfolder] = {}
+            current_folder = current_folder[subfolder]
+        return current_folder
+
+    def mkdir(self, path: Path):
+        """
+        Create a directory.
+
+        Parameters
+        ----------
+        path: Path
+            The full path of the directory.
+        """
+        path = self._relative_path(path)
+        self._cd(path)
+
+    def save_file(self, file: Path, content: Union[str, bytes]) -> None:
+        """
+        Save a file with given content.
+
+        Parameters
+        ----------
+        file : Path
+            The file including its full path and filename.
+        content : str or bytes
+            The content of the file (text or binary).
+        """
+        file = self._relative_path(file)
+        dir_ref = self._cd(file.parent)
+        if isinstance(content, str):
+            content = content.encode()
+        dir_ref[file.name] = BytesIO(content)
+
+    def write_all_to_disk(self, root: Path) -> None:
+        """
+        Write the directory structure and all files to disk.
+
+        Parameters
+        ----------
+        root : Path
+            The reference folder on the disk that becomes root.
+        """
+        self._write_recursively(root, self.directory_dict)
+
+    def _write_recursively(self, base: Path, subdirectory: dict) -> None:
+        """
+        Write everything recursively to the disk.
+
+        Parameters
+        ----------
+        base: Path
+            The reference point on the disk.
+        subdirectory : dict
+            The dict with only items in this subdirectory.
+        """
+        Path.mkdir(base, parents=True, exist_ok=True)
+        for name, obj in subdirectory.items():
+            full_path = base / Path(name)
+            if isinstance(obj, dict):
+                self._write_recursively(full_path, obj)
+            elif isinstance(obj, BytesIO):
+                with open(full_path, "wb") as f:
+                    f.write(obj.getvalue())
 
 
-def _determine_destination(destination: str, origin: str, filename: str) -> str:
-    """Determine the destination of the bundle."""
-    app_filename = None
-    if destination == "executable":
-        app_filename = os.path.join(origin, filename)
-    elif destination == "system":
-        app_filename = os.path.join("/Applications", filename)
-    elif destination == "user":
-        app_filename = os.path.join(os.path.expanduser("~"), "Applications", filename)
-    assert app_filename is not None
-    return app_filename
+class ApplicationBundle(_FilesystemDictionary):
+    """Create application bundle and manag content."""
 
+    def __init__(self, executable: Path) -> None:
+        """
+        Store the executable and plist in the correct directories.
 
-def do_the_bundle(
-    app_executable: str,
-    app_filename: Optional[str] = None,
-    app_CFBundleDisplayName: Optional[str] = None,
-    app_destination: str = "executable",
-    app_CFBundleIconFile: Optional[str] = None,
-    app_extension: Optional[str] = None,
-    app_CFBundleTypeRole: str = "Viewer",
-    app_terminal: bool = False,
-) -> str:
-    """
-    Create the execution bundle from an executable.
+        Parameters
+        ----------
+        executable : Path
+            The full path and name of the executable to be bundled.
+        """
+        super().__init__()
+        self.original_path = executable.parent
+        self.clean_executable = re.sub(r"[^A-Za-z0-9\.-]+", "", executable.name)
+        self.set_destination("executable")
+        self.set_filename(self.clean_executable)
+        self.mkdir(Path("Contents") / Path("Resources"))
+        script = Path(executable).read_bytes()
+        self.save_file(Path("Contents") / Path("MacOS") / self.clean_executable, script)
+        self.plist_dict = dict(CFBundleExecutable=self.clean_executable)
+        self.plist_dict.update(CFBundlePackageType="APPL")
+        self.set_CFBundleDisplayName(self.clean_executable + ".app")
+        self.set_CFBundleIdentifier(self.clean_executable)
+        self.CFBundleTypeRole = "Viewer"
 
-    Parameters
-    ----------
-    app_executable : str
-        The filename of the (existing) executable file to be bundled.
-    app_filename : str or None, default : None
-        The filename of the app to be generated (without .app). Defaults
-        to app_executable + '.app'
-    app_CFBundleDisplayName : str or None, default : None
-        Specifies the display name of the bundle, visible to users and
-        used by Siri.
-    app_destination : str, default = 'executable'
-        The destination of the .app file. Can be 'user'
-        (~/Applications), 'system' (/Application) or 'executable'
-        (same as app_executable).
-    app_CFBundleIconFile : str or None, default : None
-        The (existing) png to be used as an icon file.
-    app_extension : str or None, default : None
-        File extension(s) to be opened by the app.
-    app_CFBundleTypeRole : str, default = 'Viewer'
-        The app’s role with respect to the file extension. Can be
-        'Editor', 'Viewer', 'Shell' or 'None'
-    app_terminal : bool, default : False
-        Always launch the app via a terminal.
-    """
-    move_file = False
+    def set_CFBundleDisplayName(self, name: str) -> None:
+        """
+        Set the 'official' name of the app used by, e.g., Siri.
 
-    if app_terminal:
-        """Write a new script to be bundled."""
-        terminal_script = (
-            "#!/bin/bash\n/usr/bin/open '" + os.path.abspath(app_executable) + "' -a Terminal"
-        )
-        terminal_filename = LAUNCHER_NAME
-        if os.path.isfile(terminal_filename):
-            print(f"{terminal_filename} already exists.")
+        Paramters
+        ---------
+        name : str
+            The name to be given.
+        """
+        self.plist_dict.update(CFBundleDisplayName=name)
+
+    def set_CFBundleIdentifier(self, identifier: str) -> None:
+        """
+        Set the reverse domain name identifier for the app.
+
+        A pseudo domain identifier is used. If the identifier would be
+        'myapplication' -> 'org.script2bundle.myapplication'.
+
+        Parameters
+        ----------
+        identifier : str
+            The last part of that identifier adhering to RFC 1035.
+        """
+        identifier = "org.script2bundle." + identifier
+        if not self._is_valid_domain(identifier):
+            print(f"{identifier} is not a valid domain name as set forth in RFC 1035.")
             sys.exit(1)
-        with open(terminal_filename, "w") as terminal_file:
-            terminal_file.write(terminal_script)
-        os.chmod(terminal_filename, 0o755)
-        if app_filename is None:
-            app_filename = app_executable
-        app_executable = terminal_filename
-        move_file = True
+        self.plist_dict.update(CFBundleIdentifier=identifier)
 
-    # CFBundleExecutable: Name of the bundle’s executable file
-    head, tail = os.path.split(app_executable)
-    # Strip 'problematic' characters
-    clean_executable = re.sub(r"[^A-Za-z0-9\.-]+", "", tail)
-    if clean_executable != tail:
-        print(
-            f"Warning: Stripping characters from filename and duplicating executable ({clean_executable})."
-        )
-        clean_filename = os.path.join(head, clean_executable)
-        shutil.copy2(app_executable, clean_filename)
-        app_executable = clean_filename
-        move_file = True
-    # start the plist file with the name of the executable
-    info_plist = dict(CFBundleExecutable=clean_executable)
+    def set_filename(self, name: str) -> None:
+        """
+        Set the filename of the application.
 
-    # The bundle needs a filename and a name to be displayed
-    if app_filename is None:
-        app_filename = clean_executable
-    app_filename = app_filename + ".app"
+        Parameters
+        ----------
+        name : str
+            The filename without '.app'.
+        """
+        self.filename = name + ".app"
 
-    if app_CFBundleDisplayName is None:
-        app_CFBundleDisplayName = app_filename
-    info_plist.update(CFBundleDisplayName=app_CFBundleDisplayName)
+    def set_destination(self, destination: str) -> None:
+        """
+        Set the destination of the bundle.
 
-    # A bundle identifier is strongly recommended
-    app_CFBundleIdentifier = "org.script2bundle." + clean_executable
-    if not _is_valid_domain(app_CFBundleIdentifier):
-        print(f"{app_CFBundleIdentifier} is not a valid domain name as set forth in RFC 1035.")
-        sys.exit(1)
-    info_plist.update(CFBundleIdentifier=app_CFBundleIdentifier)
+        Parameters
+        ----------
+        destination : str
+            Can be 'executable' (same as input file), 'user'
+            (~/Applications) or 'system (/Applications).
+        """
+        if destination == "executable":
+            self.destination = self.original_path
+        elif destination == "system":
+            self.destination = Path("/Applications")
+        elif destination == "user":
+            self.destination = Path.home() / "Applications"
 
-    # It is an application (not, e.g., a framework)
-    info_plist.update(CFBundlePackageType="APPL")
+    def set_icon(self, icon: Path) -> None:
+        """
+        Set the icon for the app.
 
-    app_filename = _determine_destination(app_destination, head, app_filename)
-
-    # Delete possible old version
-    if os.path.isdir(app_filename):
-        shutil.rmtree(app_filename)
-
-    # Generate the directory framework
-    contents_dir = os.path.join(app_filename, "Contents")
-    macos_dir = os.path.join(contents_dir, "MacOS")
-    resources_dir = os.path.join(contents_dir, "Resources")
-    os.makedirs(macos_dir, exist_ok=True)
-    os.makedirs(resources_dir, exist_ok=True)
-
-    # copy the executable in the correct place
-    if move_file:
-        shutil.move(app_executable, macos_dir)
-    else:
-        shutil.copy(app_executable, macos_dir)
-
-    # Add the optional icon file if requested
-    if app_CFBundleIconFile is not None:
-        iconsfile = app_CFBundleIconFile + ".icns"
-        # generate the proper filetype from a png
+        icon : Path
+            The directory and filename of the icon in 'png' format.
+        """
+        # if icon.name[-4:] == ".png":
+        #      iconsfile = Path(icon.name[:-4] + ".icns")
+        # else:
+        iconsfile = Path(icon.name + ".icns")
         icon_img = icnsutil.IcnsFile()
-        icon_img.add_media(file=app_CFBundleIconFile)
-        icon_img.write(iconsfile)
-        head, tail = os.path.split(iconsfile)
-        # move the icon file in the correct place and update plist
-        shutil.move(iconsfile, resources_dir)
-        info_plist.update(CFBundleIconFile=tail)
+        icon_img.add_media(file=icon)
+        with NamedTemporaryFile(suffix=".icns") as tmp:
+            icon_img.write(tmp.name)
+            tmp.seek(0)
+            icns_bytes = tmp.read()
+        self.save_file(Path("Contents") / Path("Resources") / iconsfile, icns_bytes)
+        self.plist_dict.update(CFBundleIconFile=iconsfile.name)
 
-    # Do the optional connection to a file extension
-    if app_extension is not None:
+    def set_extension(self, extension: str) -> None:
+        """
+        Associate a file extension with the application.
+
+        Then, a double-click or 'open' on a file with that extension
+        will subsequently launch the application. The application has to
+        run at least once to 'register' the extension with the system.
+
+        Parameters
+        ----------
+        extension : str
+            ZThe extension with the preceeding period.
+        """
+        self.extension = extension
+        app_CFBundleIdentifier = self.plist_dict["CFBundleIdentifier"]
         UTTypeIdentifier = app_CFBundleIdentifier + ".datafile"
-        if not _is_valid_domain(UTTypeIdentifier):
+        if not self._is_valid_domain(UTTypeIdentifier):
             print(f"{UTTypeIdentifier} is not a valid domain name as set forth in RFC 1035.")
             sys.exit(1)
-
-        file_type = app_CFBundleDisplayName + " datafile"
-
+        file_type = self.plist_dict["CFBundleDisplayName"] + " datafile"
         app_CFBundleDocumentTypes = [
             {
                 "LSItemContentTypes": [UTTypeIdentifier],
                 "CFBundleTypeName": file_type,
-                "CFBundleTypeRole": app_CFBundleTypeRole,
+                "CFBundleTypeRole": self.CFBundleTypeRole,
             }
         ]
-
-        info_plist.update(CFBundleDocumentTypes=app_CFBundleDocumentTypes)
-
+        self.plist_dict.update(CFBundleDocumentTypes=app_CFBundleDocumentTypes)
         app_UTExportedTypeDeclarations = [
             {
                 "UTTypeIdentifier": UTTypeIdentifier,
-                "UTTypeTagSpecification": {"public.filename-extension": app_extension},
+                "UTTypeTagSpecification": {"public.filename-extension": extension},
                 "UTTypeConformsTo": "public.data",
                 "UTTypeDescription": file_type,
             }
         ]
+        self.plist_dict.update(UTExportedTypeDeclarations=app_UTExportedTypeDeclarations)
 
-        info_plist.update(UTExportedTypeDeclarations=app_UTExportedTypeDeclarations)
+    def set_CFBundleTypeRole(self, role: str):
+        """
+        Set the bundle type role.
 
-    # Write the Info.plist file
-    info_filename = os.path.join(contents_dir, "Info.plist")
-    with open(info_filename, "wb") as infofile:
-        plistlib.dump(info_plist, infofile)
+        Parameters
+        ----------
+        role : str
+            Can be 'Viewer','Editor','Shell', or 'None'.
+        """
+        self.CFBundleTypeRole = role
 
-    return app_filename
+    def write_bundle(self) -> Path:
+        """
+        Write the bundle to the disk.
+
+        Returns
+        -------
+        Path
+            The path and filename of the application bundle.
+        """
+        destination = self.destination / Path(self.filename)
+        if destination.exists():
+            shutil.rmtree(destination)
+        plist = plistlib.dumps(self.plist_dict)
+        self.save_file(Path("Contents") / Path("Info.plist"), plist)
+        self.write_all_to_disk(destination)
+        executable = destination / Path("Contents") / Path("MacOS") / self.clean_executable
+        os.chmod(executable, 0o755)
+        return destination
+
+    def _is_valid_domain(self, domain: str) -> bool:
+        """
+        Check the validity of the Uniform Type Identifiers.
+
+        Parameters
+        ----------
+        domain : str
+            The domain name to be checked.
+
+        Returns
+        -------
+        bool
+            True if the domain is valid (RFC1035) and False otherwise.
+        """
+        rfc1035_chars = string.ascii_lowercase + string.digits + "-."
+        if not all(char in rfc1035_chars for char in domain.lower()):
+            return False
+        if len(domain) > 253:
+            return False
+        if "--" in domain:
+            return False
+        if ".." in domain:
+            return False
+        return True
 
 
 def _create_argparser():
@@ -343,22 +464,42 @@ def _create_example() -> str:
     return executable
 
 
+def _create_launcher(executable: Path) -> Path:
+    """Create an launcher file and return its filename."""
+    terminal_script = f"#!/bin/bash\n/usr/bin/open '{executable.resolve()}' -a Terminal"
+    terminal_filename = LAUNCHER_NAME
+    with open(terminal_filename, "w") as terminal_file:
+        terminal_file.write(terminal_script)
+    os.chmod(terminal_filename, 0o755)
+    executable = Path(terminal_filename)
+    return executable
+
+
 def main():
     """Parse the command line and run the app."""
     args = _create_argparser()
     app_executable = args.executable
     if app_executable is None:
         app_executable = _create_example()
-    appname = do_the_bundle(
-        app_executable,
-        app_filename=args.filename,
-        app_CFBundleDisplayName=args.CFBundleDisplayName,
-        app_destination=args.destination,
-        app_CFBundleIconFile=args.CFBundleIconFile,
-        app_extension=args.extension,
-        app_CFBundleTypeRole=args.CFBundleTypeRole,
-        app_terminal=args.terminal,
-    )
+    executable = Path(app_executable)
+    if args.terminal:
+        executable = _create_launcher(executable)
+    vfs = ApplicationBundle(executable)
+    if args.destination:
+        vfs.set_destination(args.destination)
+    if args.filename:
+        vfs.set_filename(args.filename)
+    else:
+        vfs.set_filename(app_executable)
+    if args.CFBundleDisplayName:
+        vfs.set_CFBundleDisplayName(args.CFBundleDisplayName)
+    if args.CFBundleIconFile:
+        vfs.set_icon(Path(args.CFBundleIconFile))
+    if args.CFBundleTypeRole:
+        vfs.set_CFBundleTypeRole(args.CFBundleTypeRole)
+    if args.extension:
+        vfs.set_extension(args.extension)
+    appname = vfs.write_bundle()
     # Launch if requested;
     # sleep required to allow the system to recognize the new app
     if args.launch:
