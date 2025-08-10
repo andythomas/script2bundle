7a11df2d15a177b9b91071f431d1606e79bb40d1
Andy Thomas <andythomas@web.de>
2025-08-10 15:46:17 +0200

    fix: remove all pytest generated files (#38)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 """Configure pytest with extra options."""
 
+import shutil
+from pathlib import Path
+
 
 def pytest_addoption(parser):
     """Add ci option (running on github) to the parser."""
@@ -13,3 +16,24 @@ import pytest  # noqa: E402
 def cirunner(request):
     """Allow to inject a bool to test functions."""
     return request.config.getoption("--cirunner")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def remvove_generated_test_files():
+    """
+    Remove all files generated during a test.
+
+    The 'function' scope makes it run after every single test function
+    and 'autotuse' lets it run without the requirement to mark the
+    functions.
+    """
+    exec_path = Path.cwd()
+    original_files = set(exec_path.glob("*"))
+    yield
+    files_after_finish = set(exec_path.glob("*"))
+    diff = files_after_finish - original_files
+    for file in diff:
+        if file.is_file():
+            file.unlink()
+        elif file.is_dir():
+            shutil.rmtree(file)
